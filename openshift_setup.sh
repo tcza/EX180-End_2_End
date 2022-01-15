d619456d255d3d0780b15f9142e21ffc64ede691
Amit Kumar Gupta <akgmt20@gmail.com>
2022-01-15 23:54:10 +0530

    Initial Added

--- openshift_setup.sh
+++ openshift_setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 sudo yum -y install unzip tar wget
 sleep 16
-unzip Openshift_uploads.zip
+unzip Openshift_Scripts.zip
 sleep 5
-tar -xvf $(pwd)/Openshift_uploads/openshift-client-linux.tar.gz 
+tar -xvf $(pwd)/Openshift_Scripts/openshift-client-linux.tar.gz 
 sleep 6
 sudo cp -pr $(pwd)/oc /usr/local/bin/
 sleep 6
-cp -pr $(pwd)/Openshift_uploads/clean_all.sh $(pwd)
-cp -pr $(pwd)/Openshift_uploads/mysql.yaml $(pwd)
-cp -pr $(pwd)/Openshift_uploads/mysql.json $(pwd)
-cp -pr $(pwd)/Openshift_uploads/mytestfile*  $(pwd)
+cp -pr $(pwd)/Openshift_Scripts/clean_all.sh $(pwd)
+cp -pr $(pwd)/Openshift_Scripts/mysql.yaml $(pwd)
+cp -pr $(pwd)/Openshift_Scripts/mysql.json $(pwd)
+cp -pr $(pwd)/Openshift_Scripts/mytestfile*  $(pwd)
 chmod 777 *
-chmod 777 $(pwd)/Openshift_uploads/*
+chmod 777 $(pwd)/Openshift_Scripts/*
 rm -rf kubectl
 rm README.md
 rm -rf oc
